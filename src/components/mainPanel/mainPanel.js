89e77903ca208a7e3027462e57d263ab808c6d7d
Prabhjyot <knowprabhjyot@gmail.com>
2020-12-25 20:07:33 +0530

    first commit

---
+++ src/components/mainPanel/mainPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainPanel extends Component {
+    render() {
+        return (
+            <div>
+                Main Panel
+            </div>
+        )
+    }
+}
+
+export default MainPanel;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
